9b5860ac0e1de88bec4405c06079342769032368
Jorgen Carlsen <jscarlsen@gmail.com>
2020-07-31 15:41:30 -0700

    Remove unneeded code from app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import './index.css';
-import axios from 'axios';
 import config from './config.json';
 import Header from './components/Header';
-
-const apiKey = config.openWeatherApiKey;
-
-// async function getData() {
-//   const response = await axios.get(
-//     `http://api.openweathermap.org/data/2.5/weather?q=Bishop&units=imperial&appid=${apiKey}`
-//   );
-
-//   return response;
-// }
-
-//const data = getData();
 
 function App() {
   return (
